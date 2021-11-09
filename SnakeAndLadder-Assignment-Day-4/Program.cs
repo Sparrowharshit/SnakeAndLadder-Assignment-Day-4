d2dc0e8af1ae371dd3e10cd53a468475c85ef3c9
Harshit <yoharshit98@gmail.com>
2021-11-09 23:56:29 +0530

    [Harshit Welcome To SnakeAndLadder Game]

---
+++ SnakeAndLadder-Assignment-Day-4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder_Assignment_Day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake and ladder game!");
+            Console.ReadLine();
+        }
+    }
+}
